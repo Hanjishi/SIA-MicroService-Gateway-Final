f707b0dad9c9f763cec8284a6122870d3b8788c8
Hanjishi <dalisay.john06@gmail.com>
2025-04-13 02:51:58 +0800

    ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -6,27 +6,38 @@ use Illuminate\Http\Response;
 
 trait ApiResponser
 {
+
+    /**
+    * Build success response
+    * @param string|array $data
+    * @param int $code
+    * @return Illuminate\Http\JsonResponse
+    */
     public function successResponse($data, $code = Response::HTTP_OK)
     {
-        return response ($data, $code)->header('Content-Type', 'application/json');
+        return response()->json(['data' => $data], $code);
     }
-    
+    /**
+    * Build error responses
+    * @param string|array $message
+    * @param int $code
+    * @return Illuminate\Http\JsonResponse
+    */
+
     public function validResponse($data, $code = Response::HTTP_OK)
     {
         return response()->json(['data' => $data], $code);
     }
 
     public function errorResponse($message, $code)
+
     {
-        $response = is_array($message) ? $message : ['error' => $message];
-        $response['code'] = $code;
-
-        return response()->json($response, $code);
+        return response()->json(['error' => $message, 'code' => $code],
+        $code);
     }
-
 
     public function errorMessage($message, $code)
     {
-        return response($message, $code)->header('Content-Type', 'application/json');
+        return response($message, $code)->header('content-type', 'application/json');
     }
 }
